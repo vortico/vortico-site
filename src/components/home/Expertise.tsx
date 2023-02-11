dc6b69eb78afc92120d86bc83710830d1a0c8dd6
José Antonio Perdiguero López <perdy@perdy.io>
2023-02-11 12:18:58 +0100

    :sparkles: Expertise section

--- src/components/home/Expertise.tsx
+++ src/components/home/Expertise.tsx
@@ -1,5 +1,13 @@
 import React, { ReactNode } from 'react'
-import { BanknotesIcon } from '@heroicons/react/24/solid'
+import {
+  AcademicCapIcon,
+  BanknotesIcon,
+  CloudIcon,
+  CodeBracketIcon,
+  CpuChipIcon,
+  CreditCardIcon,
+  PencilSquareIcon,
+} from '@heroicons/react/24/solid'
 
 interface ExpertiseItemProps {
   icon: ReactNode
@@ -10,7 +18,7 @@ interface ExpertiseItemProps {
 function ExpertiseItem({ icon, title, description }: ExpertiseItemProps) {
   return (
     <li className="group list-item">
-      <span className="absolute -left-4 flex h-8 w-8 items-center justify-center rounded-full bg-brand-500 ring-2 ring-white ring-brand-600">
+      <span className="absolute -left-4 -ml-px flex h-8 w-8 items-center justify-center rounded-full bg-brand-500 ring-2 ring-white ring-brand-600">
         {icon}
       </span>
       <h3 className="text-2xl font-bold">{title}</h3>
@@ -25,41 +33,47 @@ export default function Expertise() {
   return (
     <section id="expertise">
       <div className="mx-auto max-w-5xl px-4 sm:px-6 md:px-8">
-        <ul className="relative list-inside space-y-6 border-l-2 border-brand-500 py-8 pl-8">
+        <h2 className="text-3xl font-bold lg:text-5xl">Expertise</h2>
+        <p className="mt-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue
+          consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis
+          id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien.
+        </p>
+        <ul className="relative mt-8 ml-4 list-inside space-y-6 border-l-2 border-brand-500 py-4 pl-8">
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<CpuChipIcon className="h-6 w-6 text-brand-600" />}
             title="Machine Learning"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<CodeBracketIcon className="h-6 w-6 text-brand-600" />}
             title="Software Development"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<CloudIcon className="h-6 w-6 text-brand-600" />}
             title="Cloud Solutions and Innovation"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
             icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
             title="WealthTech"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<CreditCardIcon className="h-6 w-6 text-brand-600" />}
             title="FinTech"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<AcademicCapIcon className="h-6 w-6 text-brand-600" />}
             title="Tech Training & Courses"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
           <ExpertiseItem
-            icon={<BanknotesIcon className="h-6 w-6 text-brand-600" />}
+            icon={<PencilSquareIcon className="h-6 w-6 text-brand-600" />}
             title="Software Architecture"
-            description="That's the description"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet felis quis mi congue consectetur. Morbi a magna viverra, fermentum lacus a, mollis sapien. Nulla orci felis, porta sit amet iaculis id, porttitor eget mauris. Curabitur odio neque, molestie id nibh id, semper tincidunt sapien."
           />
         </ul>
       </div>
--- src/components/home/MissionVision.tsx
+++ src/components/home/MissionVision.tsx
@@ -4,46 +4,48 @@ import { LightBulbIcon, RocketLaunchIcon } from '@heroicons/react/24/outline'
 export default function MissionVision() {
   return (
     <section id="mission-vision">
-      <div className="mx-auto flex max-w-5xl flex-col gap-y-16 px-4 sm:px-6 md:px-8">
-        <div className="flex flex-1 flex-col gap-y-4 md:flex-row">
-          <div className="md:basis-3/4">
-            <h2 className="text-2xl font-bold lg:text-3xl">
-              Transform the ML arena by creating an environment where the development and deployment of products is as
-              simple, straightforward, and resilient as possible.
-            </h2>
-          </div>
-          <div className="flex items-center justify-center md:basis-1/4">
-            <div className="flex h-20 w-20 flex-none overflow-hidden rounded-full p-[0.1875rem] shadow ring-1 ring-primary-900/30 dark:bg-brand-600 lg:h-28 lg:w-28">
-              <div className="flex flex-1 items-center justify-center rounded-full bg-brand-500/90 p-3 text-primary-100 dark:text-brand-600 lg:p-4">
-                <LightBulbIcon />
+      <div className="mx-auto max-w-5xl px-4 sm:px-6 md:px-8">
+        <div className="flex flex-col gap-y-16">
+          <div className="flex flex-1 flex-col gap-y-4 md:flex-row">
+            <div className="md:basis-3/4">
+              <h2 className="text-2xl font-bold lg:text-3xl">
+                Transform the ML arena by creating an environment where the development and deployment of products is as
+                simple, straightforward, and resilient as possible.
+              </h2>
+            </div>
+            <div className="flex items-center justify-center md:basis-1/4">
+              <div className="flex h-20 w-20 flex-none overflow-hidden rounded-full p-[0.1875rem] shadow ring-1 ring-primary-900/30 dark:bg-brand-600 lg:h-28 lg:w-28">
+                <div className="flex flex-1 items-center justify-center rounded-full bg-brand-500/90 p-3 text-primary-100 dark:text-brand-600 lg:p-4">
+                  <LightBulbIcon />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-1 flex-col gap-y-4 md:flex-row">
-          <div className="md:basis-3/4">
-            <p className="text-lg">Our mission is to simplify the building and deployment of ML solutions by:</p>
-            <ul className="mt-4 list-inside list-disc space-y-4">
-              <li>
-                Providing the ML community with a comprehensive set of{' '}
-                <span className="text-brand-500">top-notch libraries and services</span> that serve as the cornerstone
-                of any ML project
-              </li>
-              <li>
-                Offering <span className="text-brand-500">specialized training</span> and technical material to equip
-                our users with the knowledge and skills they need to successfully build robust ML products, as well as
-                broader Data Engineering and Software Engineering principles
-              </li>
-              <li>
-                Utilizing a <span className="text-brand-500">proprietary consulting squad</span> that is capable of
-                elevating any ML project to new heights through their expertise and innovative solutions.
-              </li>
-            </ul>
-          </div>
-          <div className="flex items-center justify-center md:order-first md:basis-1/4">
-            <div className="flex h-20 w-20 flex-none overflow-hidden rounded-full p-[0.1875rem] shadow ring-1 ring-primary-900/30 dark:bg-brand-600 lg:h-28 lg:w-28">
-              <div className="flex flex-1 items-center justify-center rounded-full bg-brand-500/90 p-3 text-primary-100 dark:text-brand-600 lg:p-4">
-                <RocketLaunchIcon />
+          <div className="flex flex-1 flex-col gap-y-4 md:flex-row">
+            <div className="md:basis-3/4">
+              <p className="text-lg">Our mission is to simplify the building and deployment of ML solutions by:</p>
+              <ul className="mt-4 list-inside list-disc space-y-4">
+                <li>
+                  Providing the ML community with a comprehensive set of{' '}
+                  <span className="text-brand-500">top-notch libraries and services</span> that serve as the cornerstone
+                  of any ML project
+                </li>
+                <li>
+                  Offering <span className="text-brand-500">specialized training</span> and technical material to equip
+                  our users with the knowledge and skills they need to successfully build robust ML products, as well as
+                  broader Data Engineering and Software Engineering principles
+                </li>
+                <li>
+                  Utilizing a <span className="text-brand-500">proprietary consulting squad</span> that is capable of
+                  elevating any ML project to new heights through their expertise and innovative solutions.
+                </li>
+              </ul>
+            </div>
+            <div className="flex items-center justify-center md:order-first md:basis-1/4">
+              <div className="flex h-20 w-20 flex-none overflow-hidden rounded-full p-[0.1875rem] shadow ring-1 ring-primary-900/30 dark:bg-brand-600 lg:h-28 lg:w-28">
+                <div className="flex flex-1 items-center justify-center rounded-full bg-brand-500/90 p-3 text-primary-100 dark:text-brand-600 lg:p-4">
+                  <RocketLaunchIcon />
+                </div>
               </div>
             </div>
           </div>
